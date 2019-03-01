b148515b4b992e626f70a9754d6500eaa663c9ff
mchlgrca <mgarcia0898@gmail.com>
2019-03-01 12:03:05 -0700

    proper index marking for questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,61 @@
 $(document).ready(function () {
 
-    // Questions
     var questions = [{
-        // [1]
+        // [0]
         question: "In what year was the lightbulb invented?",
         choices: [1870, 1873, 1876, 1879],
         answer: 1879
     },
     {
-        // [2]
+        // [1]
         question: "In what year was the telephone invented?",
         choices: [1864, 1876, 1888, 1900],
         answer: 1876
     },
     {
-        // [3]
+        // [2]
         question: "In what year was the car invented?",
         choices: [1885, 1890, 1895, 1900],
         answer: 1885
     },
     {
-        // [4]
+        // [3]
         question: "In what year was the internet invented?",
         choices: [1980, 1981, 1982, 1983],
         answer: 1983
     },
     {
-        // [5]
+        // [4]
         question: "In what year was the television invented?",
         choices: [1920, 1927, 1934, 1941],
         answer: 1927
     },
     {
-        // [6]
+        // [5]
         question: "In what year was the radio invented?",
         choices: [1880, 1895, 1910, 1925],
         answer: 1895
     },
     {
-        // [7]
+        // [6]
         question: "In what year was the computer invented?",
         choices: [1932, 1934, 1936, 1938],
         answer: 1936
     },
     {
-        // [8]
+        // [7]
         question: "In what year was the iPhone invented?",
         choices: [2001, 2003, 2005, 2007],
         answer: 2007
     },
     {
-        // [9]
+        // [8]
         question: "In what year was the microwave invented?",
         choices: [1926, 1936, 1946, 1956],
         answer: 1946
     },
     {
-        // [10]
+        // [9]
         question: "In what year was the camera invented?",
         choices: [1812, 1816, 1820, 1830],
         answer: 1816
@@ -72,6 +71,7 @@ $(document).ready(function () {
         for (i = 0; i < questions.length; i++) {
             $(".jumbotron").append(questions[i].question);
 
+            if ();
         }
 
     });
